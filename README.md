25e89137688c3c2fbb94e17258563e0514092b44
Mitchell Quinn <mquinn@kapitus.com>
2020-02-07 00:58:03 -0500

    did a little prep work, added in postman files, update the dashboard for info.

--- README.md
+++ README.md
@@ -11,3 +11,22 @@
 7. npm run dev
 
 Should only take you a few minutes to setup.
+
+---
+
+## Backend Engineer Challenge
+
+1. Parse ​ Top 10​ Holdings, Country Weights and Sector Weights, ETF name and fund
+description information for all requested ​ SPDR ETFs (​ www.spdrs.com​ ) and store them in
+a database. The information should be updated daily.
+
+2. Create API using an authentication method of your choice. However, ​ non expirable
+keys are not acceptable​ . Endpoints should include:
+
+    a. Login authentication for API access
+    
+    b. List of available ETF symbol
+    
+    c. Get data for ETF by ticker
+    
+3. Provide a ​ Postman​ file for API documentation
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,21 @@
                     <div class="card-header">{{__('Dashboard')}}</div>
 
                     <div class="card-body">
-                        {{__('Your application\'s dashboard.')}}
+                        <h1>Backend Engineer Challenge</h1>
+                        <p>
+                            1. Parse ​ Top 10​ Holdings, Country Weights and Sector Weights, ETF name and fund
+                            description information for all requested ​ SPDR ETFs (​ www.spdrs.com​ ) and store them in
+                            a database. The information should be updated daily.
+                            <br>
+                            2. Create API using an authentication method of your choice. However, ​ non expirable
+                            keys are not acceptable​ . Endpoints should include:
+                            <br>
+                                a. Login authentication for API access
+                            <br>
+                                b. List of available ETF symbol
+                            <br>
+                                c. Get data for ETF by ticker
+                        </p>
                     </div>
                 </div>
             </div>
